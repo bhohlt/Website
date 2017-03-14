80a8bbf26517413aaf7743114fcfeb9b87bffd21
bhohlt <thiemo.hohl@bbcag.ch>
2017-03-14 11:39:26 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Webdesign</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+</head>
+<body>
+<div class="header">
+<a href="../index.php"><img src="logo.png"></a>
+
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+  </li>
+</ul>
+</div class="header">
+<img id="bild01" src="bild01.png">
+<img src="http://s.cdpn.io/3/pacman-ghost-hi_1.png" class="mover">
+</body>
+</html>
