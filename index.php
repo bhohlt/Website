0067cbe59f12dee383e536aaf9c1b57f69df44fa
bhohlt <thiemo.hohl@bbcag.ch>
2017-03-15 14:47:35 +0100

    Update responsive design navbar
    evtl. Pfadangabe anpassen.

--- index.php
+++ index.php
@@ -1,27 +1,37 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Webdesign</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>HTML</title>
+	<link rel="stylesheet" type="text/css" href="CSS/style.css">
 	<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+	
+	<script type="text/javascript">
+		$(document).ready(function() {
+			$('.menubutton').click(function() {
+				$('nav').slideToggle('slow');
+			});
+		});
+	</script>
+
 </head>
 <body>
 <div class="header">
-<a href="../index.php"><img src="res/logo.png"></a>
+	<section id="menubar">
+		<ul>
+			<li><a class="menubutton" href="#menu"><img src="/res/navi.png"></a></li>
+		</ul>
+	</section>
 
-<ul>
-  <li><a href="#home">Home</a></li>
-  <li><a href="#news">News</a></li>
-  <li class="dropdown">
-    <a href="javascript:void(0)" class="dropbtn">Dropdown</a>
-    <div class="dropdown-content">
-      <a href="#">Link 1</a>
-      <a href="#">Link 2</a>
-      <a href="#">Link 3</a>
-    </div>
-  </li>
-</ul>
+	<nav class="nav">
+		<ul>
+			<li><a href="index.php" class="active">Home</a></li>
+			<li><a href="php/employ.php">Webdesign</a></li>
+			<li><a href="php/gallery.php">Referenzen</a></li>
+			<li><a href="php/contact.php">Team</a></li>
+			<li><a href="php/contact.php">Kontakt</a></li>
+		</ul>
+	</nav>
 </div class="header">
 <div style="width: 100%; height: 500px; margin-top: 25%; background-color: rgba(255,255,255,0.4);text-align: center"><h1>Thiemo</h1></div>
 <div style="width: 100%; height: 500px; margin-top: 25%; background-color: rgba(255,255,255,0.4);text-align: center"><h1>David</h1></div>
